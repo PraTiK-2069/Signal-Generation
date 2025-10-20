3964dd4492b6be1ddf913642d1a5ea9f329a1344
PraTiK-2069 <pk2286@nyu.edu>
2025-10-20 12:13:51 +0000

    Stratagy

---
+++ signals/strategy.py
@@ -0,0 +1,74 @@
+import numpy as np
+from typing import Sequence
+
+class BreakoutSignal:
+    def _init_(self, lookback:int):
+        self.lookback = lookback
+    
+    def generate(self, prices: Sequence[float]) -> np.ndarray:
+        prices = np.array(prices)
+        signals = np.zeros_like(prices, dtype=int)
+
+        for i in range(self.lookback, len(prices)):
+            window = prices[i-self.lookback:i]
+            signals[i] = np.where(prices[i]>window.max(), 1, np.where(prices[i] < window.min(), -1, 0))
+        return signals
+    
+class MovingAverageCrossOver:
+    def _init_(self, short:int, long:int):
+        self.short = short
+        self.long = long
+    
+    def moving_average(self, prices: Sequence[float], window: int) -> np.ndarray:
+        prices = np.array(prices, dtype=float)
+        ma = np.zeros_like(prices, dtype=float)
+        for i in range(len(prices)):
+            if i+1 < window:
+                ma[i] = 0 
+            else:
+                ma[i] = np.mean(prices[i+1-window:i+1])
+        return ma
+
+    def generate_ma_signals(self, prices: Sequence[float]) -> np.ndarray:
+        short_ma = self.moving_average(prices, self.short)
+        long_ma = self.moving_average(prices, self.long)
+        signals = np.where(short_ma > long_ma, 1,
+                np.where(short_ma < long_ma, -1, 0))
+        return signals
+    
+class SignalCombiner:
+    def _init_(self):
+        return
+    
+    def combine(self, signal1, signal2):
+        final_signal = []
+        for sig1, sig2 in zip(signal1, signal2):
+            final_signal.append((sig1+sig2)//2)
+        return final_signal
+
+     
+
+            
+
+
+if _name_ == "_main_":
+    prices = [100, 101, 102, 101, 99, 98, 100, 102]
+    lookback = 2
+    breakout = BreakoutSignal(lookback)
+    signals_breakout = breakout.generate(prices)
+    # print("Prices:  ", prices)
+    print("signals_breakout: ", signals_breakout.tolist())
+
+    prices = [100, 101, 102, 101, 99, 98, 100, 102]
+    short_window = 2
+    long_window = 3
+
+    mac = MovingAverageCrossOver(short_window, long_window)
+    signals_mac = mac.generate_ma_signals(prices)
+
+    # print("Prices:  ", prices)
+    print("signals_mac     : ", signals_mac.tolist())
+
+    signal_combiner = SignalCombiner()
+    final_signal = signal_combiner.combine(signals_breakout, signals_mac)
+    print("final signal    : ", final_signal)
